13fc90d11b1cb4017f446e074b872c5d50e1ce7c
gatox <salazar.xavier@gmail.com>
2025-06-30 16:07:27 +0200

    Change self.rescale_vel in prop_dynamics.py

--- pysurf/fssh/prop_dynamics.py
+++ pysurf/fssh/prop_dynamics.py
@@ -86,7 +86,7 @@ class State(Colt):
         self.states = states
         self.ncoeff = ncoeff
         self.prob = prob
-        self.rescale_vel = config["rescale_vel"].value
+        self.rescale_vel = rescale_vel
         if config["rescale_vel"] == "momentum":
             self.reduced_kene = config["rescale_vel"]["number_vdf"]
         self.coupling = coupling
